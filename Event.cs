df2ac3820650f7fb8708bff5fe9910961ae698c5
clafish <andrew.korzh2008@gmail.com>
2026-04-09 18:11:44 +0300

    first classes

---
+++ Event.cs
@@ -0,0 +1,10 @@
+﻿namespace pair;
+
+public class Event
+{
+    public int Move;
+    public string Description { get; }
+    public string Type { get; }
+    public int CharacteristicChange { get; }
+    
+}
---
+++ Hero.cs
@@ -0,0 +1,21 @@
+﻿namespace pair;
+
+public class Hero
+{
+    public string Name { get; }
+    public string Role { get; }
+    public int Lvl { get; }
+    public int HP { get; }
+    public int Money { get; }
+    public string State { get; }
+
+    public Hero(string name, string role)
+    {
+        Name = name;
+        Role = role;
+        Lvl = new Random().Next(1, 50);
+        HP = 100;
+        Money = new Random().Next(100, 5000);
+        State = "alive";
+    }
+}
